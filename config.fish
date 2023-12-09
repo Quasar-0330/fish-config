5be864da4a1dab730269c21855b91520a5da2a9f
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2023-12-09 20:20:50 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,1 +1,20 @@
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+alias DL='cd /home/yuto/ダウンロード'
+alias rb='sudo reboot'
+alias sd='sudo poweroff'
+#exaをインストールしてね
+alias la='lsd -a'
+alias ls='lsd'
+alias ll='lsd -l'
+alias sudo='sudo '
+alias aud='apt update'
+alias aug='apt upgrade'
+alias ais='apt install'
+alias arm='apt --purge remove'
+alias e='nvim'
+alias mc='sudo ifconfig wlp2s0 down && sudo macchanger -r wlp2s0 && sudo ifconfig wlp2s0 up'
+export EDITOR=nvim
+export VISUAL="$EDITOR"
 
+fish_vi_key_bindings
+#テーマ oh-my-fish/theme-bobtherfish
