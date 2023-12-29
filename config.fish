e4e3dfc5d4865d2e7f69244eec29bf4487f16b22
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2023-12-30 03:33:29 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,19 +1,14 @@
 alias DL='cd /home/yuto/ダウンロード'
 alias rb='sudo reboot'
 alias sd='sudo poweroff'
-
 alias la='lsd -a'
 alias ls='lsd'
 alias ll='lsd -l'
-alias sudo='sudo '
-alias aud='apt update'
-alias aug='apt upgrade'
-alias ais='apt install'
-alias arm='apt --purge remove'
 alias e='nvim'
-alias mc='sudo ifconfig wlp2s0 down && sudo macchanger -r wlp2s0 && sudo ifconfig wlp2s0 up'
+alias pp='sudo powerpill'
+alias rm='rm -i'
 export EDITOR=nvim
 export VISUAL="$EDITOR"
-
+set -g theme_display_user yes
+set -g theme_display_hostname yes
 fish_vi_key_bindings
-#テーマ oh-my-fish/theme-bobthefish
