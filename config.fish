dd76f598743ba1b8741a59b393cbfb4707825846
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2024-01-08 23:17:00 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -4,10 +4,5 @@ alias sd='sudo poweroff'
 alias la='lsd -a'
 alias ls='lsd'
 alias ll='lsd -l'
-alias e='nvim'
-alias pp='sudo powerpill'
-alias rm='rm -i'
-export EDITOR=nvim
-export VISUAL="$EDITOR"
 set -g theme_display_user yes
 set -g theme_display_hostname yes
